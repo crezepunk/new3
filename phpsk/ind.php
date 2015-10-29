5dd7745e15403f964837d91063b4603bca2e8364
crezepunk <andrey1995-2009@mail.ru>
2015-10-29 13:47:48 +1000

    Site with PHP script

---
+++ phpsk/ind.php
@@ -0,0 +1,29 @@
+<html>
+  <head>
+    <title>Блочная верстка</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+	<div id="container">
+        	<div id="header">Шапка сайта</div>
+		<div id="menu">
+			<div id="m11"><a id="m21" href="http://www.wwe.com/superstars/champions"><img src="images/champs.jpg"></a></div>
+			<div id="m12"><a id="m21" href="http://www.youtube.com/user/WWEFanNation"><img src="images/YOU.jpg"></a></div>
+			<div id="m13"><a id="m21" href="https://ru.wikipedia.org/wiki/%D0%A1%D0%BF%D0%B8%D1%81%D0%BE%D0%BA_pay-per-view_%D1%88%D0%BE%D1%83_WWE"><img src="images/PPV.jpg"></a></div>
+			</div>
+			<div id="menu2">
+			<div id="m21"><a id="m21" href="http://www.wwe.com/wwenetwork"><img src="images/m21.jpg"></a></div>
+			<div id="m22"><a id="m21" href="http://www.wwe.com/shows/wwenxt"><img src="images/m22.jpg"></a></div>
+			<div id="m23"><a id="m21" href="http://www.wrestlingfederation.ru/"><img src="images/m23.jpg"></a></div>
+		</div>
+        	<div id="content"><font size=3 color="white"><b>World Wrestling Entertainment, Inc.</b> (также известна как WWE) — американская компания, занимающаяся проведением мероприятий профессионального рестлинга, а также выпускающая кинопродукцию, музыкальные диски, одежду и компьютерные игры. Основана Родериком Джеймсом МакМэном и Тутсом Мондтом в 1952 году и сначала называлась Capitol Wrestling Corporation. Сейчас World Wrestling Entertainment (WWE) является крупнейшей в Америке и во всём мире федерацией профессионального рестлинга; число любителей данного вида спорта только в США составляет 13 миллионов человек, а мероприятия транслируется в более чем 150 странах.<br>
+								В настоящее время компанией управляет Винс Макмэн. Вместе со своей женой Линдой и детьми Шейном и Стефани управляет примерно 70 % финансовой составляющей компании и их поддерживают 96 % голосов сотрудников, имеющих право голосовать. Штаб-квартира компании расположена в городе Стэмфорде, штат Коннектикут; имеются офисы в Нью-Йорке, Лос-Анджелесе, Токио, Шанхае, Сингапуре, Лондоне и в Мумбаи. World Wrestling Entertainment за свою историю несколько раз меняла название: Capitol Wrestling Entertainment, потом World Wide Wrestling Federation, World Wrestling Federation, World Wrestling Federation Entertainment, World Wrestling Entertainment и наконец в 2011 году её сократили просто до WWE.<br> 
+								<?php
+		echo rand(1,100); //Генерируются от 1 до 100
+	?></font></div> 
+        	<div id="clear"></div>                     
+        	<div id="footer"></div>
+        </div>
+ 
+</body>
+</html>
